24fda4b3db633080cd2aa06677a54024b90eb56a
reetam100 <reetam.chatterjee@fortmindz.in>
2024-08-18 11:38:50 +0530

    fixing the issues with .env

--- src/lib/mongoClient.ts
+++ src/lib/mongoClient.ts
@@ -1,11 +1,11 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-if (!process.env.local.MONGO_URI) {
+if (!process.env.MONGO_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-const uri = process.env.local.MONGO_URI as string;
+const uri = process.env.MONGO_URI as string;
 const options = {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -16,7 +16,7 @@ const options = {
 
 let client: MongoClient;
 
-if (process.env.local.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   let globalWithMongo = global as typeof globalThis & {
